177bd3e6338b2237a12ccafcb44939e5dd0ecbd4
Root <root@root.com>
2013-11-12 16:03:00 +0400

    frontend ajax

--- app.js
+++ app.js
@@ -109,7 +109,76 @@ app.delete('/user/:id', function(req, res) {
 	});
 });
 
+// List of skills
+app.get('/skills', function(req, res) {
+	return Skill.find(function(err, skills) {
+		if (!err) {
+			return res.send(skills);
+		} else {
+			return console.log(err);
+		}
+	});
+});
 
+// Get spesific skill
+app.get('/skill/:id', function(req, res) {
+	return Skill.findById(req.params.id, function(err, skill) {
+		if (!err) {
+			return res.send(skill);
+		} else {
+			return console.log(err);
+		}
+	});
+});
+
+// Create skill
+app.post('/skills', function(req, res) {
+	console.log('POST:');
+	console.log(req.body);
+	var skill = new Skill({
+		name: req.body.name,
+		email: req.body.email,
+		pass: req.body.pass
+	});
+	skill.save(function(err) {
+		if (!err) {
+			return console.log("created");
+		} else {
+			return console.log(err);
+		}
+	});
+	return res.send(skill);
+});
+
+// Update skill
+app.put('/skill/:id', function(req, res) {
+	return Skill.findById(req.params.id, function(err, skill) {
+		skill.name = req.body.name;
+		skill.email = req.body.email;
+		skill.pass = req.body.pass;
+		return skill.save(function(err) {
+			if (!err) {
+				return res.send(skill);
+			} else {
+				return console.log(err);
+			}
+		});
+	});
+});
+
+// Delete skill
+app.delete('/skill/:id', function(req, res) {
+	return Skill.findById(req.params.id, function(err, skill) {
+		return skill.remove(function(err) {
+			if (!err) {
+				console.log("removed");
+				return res.send('removed');
+			} else {
+				console.log(err);
+			}
+		});
+	});
+});
 
 app.listen(3000, function() {
 	console.log('Listening on 3000 port');
